0f65898cdae1557971d5a9e2e2930b604be81e21
Pukar Sharma <sharmapukar217@gmail.com>
2024-01-08 20:31:19 +0545

    perf: disabled fade effect

--- src/lib/components/section.svelte
+++ src/lib/components/section.svelte
@@ -14,7 +14,7 @@
   }>();
 </script>
 
-<section id="section-{id}" class={twMerge("not-prose scroll-mt-[72px] fade-in", className)}>
+<section id="section-{id}" class={twMerge("not-prose scroll-mt-[72px]", className)}>
   <div
     class={twMerge(
       "relative mx-auto px-4 md:px-6 py-12 md:py-14 lg:py-16 max-w-5xl",
