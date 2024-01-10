3ad7d5cb50267ba77407f7ceb5bf5a29c2dd02a6
Pukar Sharma <sharmapukar217@gmail.com>
2024-01-10 13:24:03 +0545

    fix: fixing dark mode color scheme

--- src/lib/components/UpdatePrompt.svelte
+++ src/lib/components/UpdatePrompt.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import t, { type Toast } from "svelte-french-toast";
-  const { toast, onUpdate } = $props<{ toast: Toast; onUpdate: () => void }>();
+
+  export let toast: Toast;
+  export let onUpdate: () => void;
 </script>
 
 <div
@@ -13,6 +15,7 @@
   <div class="grid grid-cols-2 gap-2">
     <div>
       <button
+        onclick={onUpdate}
         class="inline-flex justify-center w-full px-2 py-1.5 rounded-md text-xs font-medium text-center bg-primary text-primary-foreground hover:bg-primary/80">
         Update</button>
     </div>
